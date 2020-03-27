86c91860e6b446bce05ebf1bc95edf5064a64624
rick0048 <rick0048@algonquinlive.com>
2020-03-27 16:30:38 -0400

    Homescreen Html/CSS

---
+++ Javascript/main.js
@@ -0,0 +1,11 @@
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.querySelector(".nav").style.top = "0";
+  } else {
+    document.querySelector(".nav").style.top = "-500px";
+  }
+  prevScrollpos = currentScrollPos;
+}
